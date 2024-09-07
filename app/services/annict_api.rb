ba78dfa4045bae02bde7a80fa6a8c03092544b87
MaTTalv001 <pomeranian1981@gmail.com>
2024-09-07 23:42:48 +0900

    いいね一覧

--- app/services/annict_api.rb
+++ app/services/annict_api.rb
@@ -28,4 +28,29 @@ class AnnictApi
       Rails.logger.error "Response body: #{response.body}"
       []
     end
+
+
+    def fetch_work(annict_id)
+        params = {
+          fields: "id,title,title_kana,season_name_text,official_site_url,twitter_username,images",
+          access_token: @access_token,
+          filter_ids: annict_id
+        }
+      
+        response = Faraday.get(BASE_URL, params)
+      
+        if response.success?
+          data = JSON.parse(response.body)
+          work = data["works"].first  # 最初の作品を取得
+          Rails.logger.info "Fetched work: #{work.inspect}"  # ログに出力
+          work
+        else
+          Rails.logger.error "Annict API error: #{response.status} #{response.body}"
+          nil
+        end
+      rescue JSON::ParserError => e
+        Rails.logger.error "JSON parse error: #{e.message}"
+        Rails.logger.error "Response body: #{response.body}"
+        nil
+      end
   end
